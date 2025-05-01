9e631e272ffe62a76fbffade1fd0370bf3979457
AnnaErsh <aershova096@gmail.com>
2025-05-01 13:27:14 +0200

    logging utils

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,8 @@ from .database import db, embed_fn
 from .processing import process_documents, save_pdf_images, save_pdf_figures, prompt
 from .compilation import CompilePresentation
 from .services import client
+from .utils import upload_files, Logger
+from .interface import demo
 
 __all__ = [
     "db",
@@ -12,4 +14,7 @@ __all__ = [
     "prompt",
     "CompilePresentation",
     "client",
+    "upload_files",
+    "Logger",
+    "demo",
 ]
---
+++ src/interface/__init__.py
@@ -0,0 +1,3 @@
+from .gradio_interface import demo
+
+__all__ = ["demo"]
