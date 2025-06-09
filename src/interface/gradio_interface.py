72a6115a167aa65d4c1407f064cfd8475854934b
KajetanNiewczas <kajetan.niewczas@uwr.edu.pl>
2025-06-09 20:14:09 +0200

    Manage errors while processing documents

--- src/interface/gradio_interface.py
+++ src/interface/gradio_interface.py
@@ -148,8 +148,8 @@ with gr.Blocks(theme=theme, js=JS_FUNC) as demo:
             )
             gr.HTML(
                 f"""
-                    <div style="display: flex; flex-direction: row; align-items: center; justify-content: center; gap: 12px; margin-bottom: 12px;">
-                        <p style="font-size: 16px;">Please ⭐️ us on GitHub!</p>
+                    <div style="display: flex; flex-direction: row; align-items: center; justify-content: center; gap: 20px; margin-bottom: 12px;">
+                        <p style="font-size: 16px; line-height: 1; margin: 0;">Please ⭐️ us on GitHub!</p>
                         <a href="https://github.com/RAGnTeX/RAGnTeX" target="_blank" style="
                             display: inline-flex;
                             align-items: center;
@@ -158,6 +158,8 @@ with gr.Blocks(theme=theme, js=JS_FUNC) as demo:
                             background-color: #4c1d95;
                             color: white;
                             font-weight: 600;
+                            font-size: 16px;
+                            line-height: 1;
                             border-radius: 8px;
                             text-decoration: none;
                             gap: 8px;
