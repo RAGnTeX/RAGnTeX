75c75ce38fc1b41b0b804568e31b79c215ec3d77
AnnaErsh <aershova096@gmail.com>
2025-06-09 21:36:42 +0200

    llm structured output

--- src/processing/__init__.py
+++ src/processing/__init__.py
@@ -3,7 +3,6 @@
 from .document_processing import delete_uploaded_files, process_documents
 from .images_processing import find_used_gfx, save_pdf_figures, save_pdf_images
 from .output_folder import create_output_folder
-from .prompt import get_prompt
 
 __all__ = [
     "process_documents",
@@ -12,5 +11,4 @@ __all__ = [
     "save_pdf_figures",
     "find_used_gfx",
     "create_output_folder",
-    "get_prompt",
 ]
