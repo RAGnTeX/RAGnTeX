06fab0d7944c5399bf4b310f4aee62fdb4f13a59
AnnaErsh <aershova096@gmail.com>
2025-06-15 19:25:58 +0200

    trying to fix temp dir in HF

--- ragntex.py
+++ ragntex.py
@@ -9,7 +9,7 @@ _ = init_telemetry()
 
 # Run Gradio
 if os.getenv("IN_DOCKER") == "true":
-    os.chdir("/cache")
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     demo.launch(
         server_name="0.0.0.0",  # nosec: intentional bind all interfaces for HuggingFace Spaces
         server_port=7860,
