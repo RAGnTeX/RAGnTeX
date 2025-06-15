a1d6c5418a4f2a6cb379dc14098ab6d7018f1a31
AnnaErsh <aershova096@gmail.com>
2025-06-15 15:40:28 +0200

    aspect ratio and model name user-defined, saveguard around generate content

--- src/interface/__init__.py
+++ src/interface/__init__.py
@@ -1,7 +1,15 @@
 """This module initializes the UI package."""
 
 from .gradio_interface import demo
-from .manage_files import upload_files, download_files, delete_files
-from .session_manager import create_session, with_update_session, check_session_status
+from .manage_files import delete_files, download_files, upload_files
+from .session_manager import check_session_status, create_session, with_update_session
 
-__all__ = ["demo", "upload_files", "download_files", "delete_files", "create_session", "with_update_session", "check_session_status"]
+__all__ = [
+    "demo",
+    "upload_files",
+    "download_files",
+    "delete_files",
+    "create_session",
+    "with_update_session",
+    "check_session_status",
+]
--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,7 +1,7 @@
 """This module initializes the services package."""
 
-from .google_client import client
+from .google_client import client, generate_with_retry
 from .output_schema import Presentation
 from .prompt import build_prompt
 
-__all__ = ["client", "build_prompt", "Presentation"]
+__all__ = ["client", "build_prompt", "Presentation", "generate_with_retry"]
